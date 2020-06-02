832c6d446e2e56e279b8a8a206f0c2a0099352c9
Reggie Johnson <52217056+rtjohnson0@users.noreply.github.com>
2020-06-02 11:38:41 -0400

    finished product 06/02

---
+++ Routes/apiProducts.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const mysql = require('mysql')
+require('dotenv').config()
+
+var connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+  
+    // Your username
+    user: process.env.USER,
+  
+    // Your password
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+  })
+
+router.get('/products',(req,res)=>{
+  connection.query('SELECT Products.product_name, Prices.price , Products.URL FROM Products INNER JOIN Prices ON Products.product_id = Prices.product_id;', function (err, data){
+    if(err){
+      throw err
+    }
+    
+      res.send(data)
+      console.log(err)
+    
+  })
+})
+
+module.exports = router
+
+
+  
--- src/components/products.js
+++ src/components/products.js
@@ -3,61 +3,48 @@ import React from 'react'
 
  export default class Product extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      products: [],
+      error : false
+    }
+  }
+componentDidMount(){
+  fetch('http://localhost:3006/api/products')
+  .then((res)=> res.json())
+  .then(
+    (data) => {
+      this.setState({
+        products: data
+      })
+    }
+    
+    
+  )
+}
     
      render(){
+       const {products} = this.state;
      return(
          <>
 
       <div class="w3-container">
-        <div class="w3-card-4">
-          <img src="img/GOAT_Shoes.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 11 | Concords</p>
-          </div>
+        {products.map((products) => (
+           <div class="w3-card-4">
+           <img src={products.URL} alt="Alps" />
+           <div class="w3-container w3-center">
+            <p>{products.product_name}</p>
+            <p className ="w3-center">${products.price}</p>
+             </div>
+        
+            </div>
+
+        ))}
+
+        
         </div>
-        <div class="w3-card-4">
-          <img src="/img/goat_7.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 7 | Bordeaux</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/infared6.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 6 | Infared</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/mettalic-5.png" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 5 | Metallic</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/top3.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 1 | Top 3</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/jordan-cg.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 11 | Cool Grey</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/bc-3.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 3 | Black Cement</p>
-          </div>
-        </div>
-        <div class="w3-card-4">
-          <img src="img/hares.jpg" alt="Alps" />
-          <div class="w3-container w3-center">
-            <p>Jordan 7 | Hares</p>
-          </div>
-        </div>
-      </div>
+        
       </>
      )
      }
